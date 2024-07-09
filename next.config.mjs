28bf4efb35e8f247f9de8d26cb527a3c0ceab81e
Shakha0410 <133149671+Shakha0410@users.noreply.github.com>
2024-07-08 23:36:41 -0400

    fsdgdfg

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ export default withSentryConfig(withSentryConfig(nextConfig, {
 // For all available options, see:
 // https://github.com/getsentry/sentry-webpack-plugin#options
 
-org: "smkweb",
+org: "privateclinic",
 project: "care-pulse",
 
 // Only print logs for uploading source maps in CI
